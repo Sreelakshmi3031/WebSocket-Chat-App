11d0b59e380f4596b966c85f2626d9f7fec252a7
Sreelakshmi3031 <245524193+Sreelakshmi3031@users.noreply.github.com>
2025-12-16 09:00:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { Server } from 'socket.io';
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'index.html'))
+})
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
+})
+
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000')
+})
